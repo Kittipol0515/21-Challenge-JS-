598e871c78809c8feb31bec960918a0c7b32dedf
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-26 19:36:53 +0700

    complete ch9

--- Parallax Scolling/start.js
+++ Parallax Scolling/start.js
@@ -1,7 +1,17 @@
 (() => {
 
-    function run() {
+    function onScroll(){
+        const moonElem = document.querySelector('.moon')
+        const wishElem = document.querySelector('.wish')
 
+        moonElem.style.transform = `translate(${window.scrollY * 1.0}%, ${window.scrollY * -1.0}%)`;
+        wishElem.style.transform = `translateY(${window.scrollY * -1.0}%`;
+
+    }
+
+
+    function run() {
+        document.addEventListener('scroll', onScroll)
     }
     run()
 })();
