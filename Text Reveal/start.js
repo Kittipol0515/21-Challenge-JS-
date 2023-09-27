62c278e9d6ef04c065a993219c97f84112ff30d9
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-27 20:41:10 +0700

    update

--- Text Reveal/start.js
+++ Text Reveal/start.js
@@ -1,10 +1,12 @@
 (() => {
 
+    function onScroll() {
+        const sectionelems = Array.fromdocument.querySelectorAll('section')
+        console.log(sectionelems)
+    }
 
     function run(){
-
+        document.addEventListener('scoll',onScroll)
     }
     run()
-
-
 })();
