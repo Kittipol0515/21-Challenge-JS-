6a5fdaa52f42a3235f06fe8cd8b35980ce892838
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-27 20:54:44 +0700

    complete ch11

--- Text Reveal/start.js
+++ Text Reveal/start.js
@@ -1,12 +1,23 @@
 (() => {
 
     function onScroll() {
-        const sectionelems = Array.fromdocument.querySelectorAll('section')
-        console.log(sectionelems)
+        const sectionElems = Array.from(document.querySelectorAll('section'))
+        
+        sectionElems.forEach((sectionElems) => {
+            const imgElem = sectionElems.querySelector('img');
+            const textElem = sectionElems.querySelector('.text');
+
+            const scrollPosition = window.pageYOffset;
+            const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 5;
+
+            if(scrollPosition >= revealPosition) {
+                textElem.classList.add('reveal');
+            }
+        })
     }
 
     function run(){
-        document.addEventListener('scoll',onScroll)
+        document.addEventListener('scroll',onScroll)
     }
-    run()
+    run();
 })();
