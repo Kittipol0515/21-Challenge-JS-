aa2d644cd2c84cb3e73dd7de1e1fd23a7ed5c08c
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-27 22:36:02 +0700

    complete ch 12

--- Air visual/start.js
+++ Air visual/start.js
@@ -1,17 +1,76 @@
 (() => {
     const KEY = '4c0096bd-0caf-404a-ad7e-5268f6887029';
 
+    async function getAirQuality({ city, state, country }) {
+        const response = await fetch(
+            `https://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=${KEY}`
+        );
 
-    function getAirQuality({ city,state,country}) {
-        
+        const { data: { current } } = await response.json();
+        const { pollution, weather } = current;
+
+        return {
+            aqi: pollution.aqius, 
+            temperature: weather.tp, 
+            humidity: weather.hu,
+            wind: weather.ws
+        }
     }
 
-    function run() {
-        const city = 'Minburi';
+    function displayAirQuality({ city, state, country, aqi, temperature, humidity, wind }) {
+        const cityElem = document.querySelector('.city');
+        const stateCountryElem = document.querySelector('.state-country');
+        const aqiElem = document.querySelector('.aqi > h1');
+        const temperatureElem = document.querySelector('.temperature'); 
+        const humidityElem = document.querySelector('.humidity');
+        const windElem = document.querySelector('.wind');
+
+        cityElem.innerText = city;
+        stateCountryElem.innerText = `${state}, ${country}`;
+        aqiElem.innerText = aqi;
+        temperatureElem.innerText = `${temperature}°C`;
+        humidityElem.innerText = `Humidity: ${humidity}%`; 
+        windElem.innerText = `Wind Speed: ${wind} m/s`; 
+    }
+
+    function setAirQualityColor(aqi) {
+        if (aqi <= 50) {
+            document.documentElement.style.setProperty(
+                `--current-aqi-color`,
+                `var(--good-aqi-color)`
+            )
+        } else if (aqi <= 100) {
+            document.documentElement.style.setProperty(
+                `--current-aqi-color`,
+                `var(--medium-aqi-color)`
+            )
+        } else {
+            document.documentElement.style.setProperty(
+                `--current-aqi-color`,
+                `var(--bad-aqi-color)`
+            ) 
+        }
+    }
+
+    async function run() {
+        const city = 'Sathon';
         const state = 'Bangkok';
         const country = 'Thailand';
 
-        getAirQuality({city, state, country})
+        const { aqi, temperature, humidity, wind } = await getAirQuality({ city, state, country });
+
+        displayAirQuality({
+            city,
+            state,
+            country,
+            aqi,
+            temperature,
+            humidity,
+            wind
+        });
+
+        setAirQualityColor(aqi)
     }
+
     run();
 })();
