80b6f19581c312ffd41717a0012adff69568d6d3
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-27 21:58:12 +0700

    progress

---
+++ Air visual/start.js
@@ -0,0 +1,17 @@
+(() => {
+    const KEY = '4c0096bd-0caf-404a-ad7e-5268f6887029';
+
+
+    function getAirQuality({ city,state,country}) {
+        
+    }
+
+    function run() {
+        const city = 'Minburi';
+        const state = 'Bangkok';
+        const country = 'Thailand';
+
+        getAirQuality({city, state, country})
+    }
+    run();
+})();
