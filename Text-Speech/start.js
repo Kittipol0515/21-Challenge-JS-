ffe6d781f8c74235fed9733240364fb8062363c4
Kittipol <kittipol.surapinchai@gmail.com>
2023-09-29 21:21:15 +0700

    complete ch 16

--- Text-Speech/start.js
+++ Text-Speech/start.js
@@ -1,4 +1,24 @@
 (() => {
-    
 
+    const message = new SpeechSynthesisUtterance();
+
+        function onVoicesChanged() {
+            const voices = speechSynthesis.getVoices();
+            const enVoice = voices.find(voiced => voices.lang === 'en-US');
+            message.voice = enVoice;
+        }
+
+        function onClick(event) {
+            message.text = event.target.getAttribute('Alt');
+            speechSynthesis.speak(message);
+        }
+
+    function run() {
+        speechSynthesis.addEventListener('voiceschanged', onVoicesChanged);
+
+        const imgElems = Array.from(document.querySelectorAll('img'));
+        imgElems.forEach(imgElems => imgElems.addEventListener('click', onClick));
+    }
+
+    run()
 })();
