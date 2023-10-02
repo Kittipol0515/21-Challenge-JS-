6a214a03c37e8330285744620fe27805c1a14d42
Kittipol <kittipol.surapinchai@gmail.com>
2023-10-02 18:41:20 +0700

    complete fix bug later ch 19

--- Autocomplete/start.js
+++ Autocomplete/start.js
@@ -1,52 +1,112 @@
+// Bugfix for
+// (() => {
+//     const carBrands = [
+//         'BMW',
+//         'Maserati',
+//         'Mercedes Benz',
+//         'Ferrari',
+//         'Toyota',
+//         'Honda',
+//         'Hyundai'
+//     ];
+
+//     const searchElem = document.querySelector('.search')
+
+//     function selectCarBrand(event) {
+//         searchElem.value = event.target.innerText
+//         clearResults();
+//     }
+
+//     function clearResults() {
+//         const ulElem = document.querySelector('.results');
+//         if(ulElem) {
+//             document.body.removeChild(ulElem)
+//         }
+//     } 
+
+//     function onInput(event) {
+//         const inputText = event.target.value.toLowerCase();
+
+//         const matchedCarBrands = carBrands.filter(carBrand => 
+//         carBrand.toLowerCase().startsWith(inputText));
+        
+//         const ulElem = document.createElement('ul');
+//         ulElem.classList.add('results');
+
+//         matchedCarBrands.forEach(carBrand => {
+//             const liElem = document.createElement('li');
+//             liElem.innerText = carBrand;
+//             liElem.onclick = selectCarBrand;
+//             ulElem.appendChild(liElem)
+//         });
+//         document.body.appendChild(ulElem)
+//     };
+
+//     function run() {
+//         searchElem.addEventListener('input', onInput);
+//         document.addEventListener('click', clearResults);
+//     }
+//     run(); 
+// })();
+
 (() => {
     const carBrands = [
-        'BMW',
-        'Maserati',
-        'Mercedes Benz',
-        'Ferrari',
-        'Toyota',
-        'Honda',
-        'Hyundai'
+      'BMW',
+      'Maserati',
+      'Mercedes Benz',
+      'Ferrari',
+      'Toyota',
+      'Honda',
+      'Hyundai'
     ];
-
-    const searchElem = document.querySelector('.search')
-
-
+    const searchElem = document.querySelector('.search');
+  
     function selectCarBrand(event) {
-        searchElem.value = event.target.innerText
-        clearResults();
+      searchElem.value = event.target.innerText;
+      clearResults();
     }
-
+  
+    function createUlElement() {
+      const ulElem = document.createElement('ul');
+      ulElem.classList.add('results');
+      return ulElem;
+    }
+  
+    function createLiElement(carBrand) {
+      const liElem = document.createElement('li');
+      liElem.innerText = carBrand;
+      liElem.onclick = selectCarBrand;
+      return liElem;
+    }
+  
     function clearResults() {
-        const ulElem = document.querySelector('.results');
-        if(ulElem) {
-            document.body.removeChild(ulElem)
-        }
+      const ulElem = document.querySelector('.results');
+      if (ulElem) {
+        document.body.removeChild(ulElem);
+      }
     }
-
-    
-
-    function onInput(event) {
-        const inputText = event.target.value.toLowerCase();
-
-        const matchedCarBrands = carBrands.filter(carBrand => 
-        carBrand.toLowerCase().startsWith(inputText));
-        
-        const ulElem = document.createElement('ul')
-        ulElem.classList.add('results')
-
-        matchedCarBrands.forEach(carBrand => {
-            const liElem = document.createElement('li');
-            liElem.innerText = carBrand;
-            liElem.onclick = selectCarBrand;
-            ulElem.appendChild(liElem)
-        });
-        document.body.appendChild(ulElem)
-    };
-
+  
+    function createResults(event) {
+      clearResults();
+  
+      const inputText = event.target.value.toLowerCase();
+      const matchedCarBrands = carBrands.filter(carBrand =>
+        carBrand.toLowerCase().startsWith(inputText)
+      );
+  
+      const ulElem = createUlElement();
+      matchedCarBrands.forEach(carBrand => {
+        const liElem = createLiElement(carBrand);
+        ulElem.appendChild(liElem);
+      });
+  
+      document.body.appendChild(ulElem);
+    }
+  
     function run() {
-        searchElem.addEventListener('input', onInput);
-        document.addEventListener('click', clearResults);
+      searchElem.addEventListener('input', createResults);
+      document.addEventListener('click', clearResults);
     }
-    run(); 
-})();
+  
+    run();
+  })();
